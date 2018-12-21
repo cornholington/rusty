1289a7fc60f1dc4f88836ac731c59e32b6b45a07
Rob Walker <rwalker@rwalker.com>
2018-12-20 18:45:37 -0800

    .

--- ledger_showdown/src/main.rs
+++ ledger_showdown/src/main.rs
@@ -14,59 +14,62 @@ const INIT: u64 = 100_000;
 const BLOCK: u64 = 100;
 const WRITES: u64 = 10_000;
 
-fn rocksdb() {
-    // rocksdb
-    extern crate rocksdb;
-    use rocksdb::{Options, DB};
-
-    let _ignored = DB::destroy(&Options::default(), "rocksdb");
-
-    let mut options = Options::default();
-    options.create_if_missing(true);
-
-    let db = DB::open(&options, "test").expect("first open");
-
-    // initialize to a big ass db
-    let data = [0u8; 1024 * 64];
-    for i in 0..INIT {
-        let mut key = [0u8; 16];
-        BigEndian::write_u64(&mut key, i);
-        db.put(&key, &data).expect(&format!("writing {} failed", i));
-    }
-
-    let mut rng = rand::thread_rng();
-    // random order for each chunk of a BLOCK
-    let now = Instant::now();
-    for i in 0..WRITES / BLOCK {
-        let mut keys: Vec<u64> = (i..i + BLOCK).collect();
-        keys.shuffle(&mut rng);
-        for i in keys {
-            let mut key = [0u8; 16];
-            BigEndian::write_u64(&mut key, i + INIT);
-            db.put(&key, &data).expect(&format!("writing {} failed", i));
-        }
-    }
-
-    println!(
-        "rocksdb validator-style writes/sec {}",
-        per_second(WRITES, &now.elapsed())
-    );
-
-    // append-style
-    let now = Instant::now();
-    for i in 0..WRITES {
-        let mut key = [0u8; 16];
-        BigEndian::write_u64(&mut key, i + INIT + WRITES);
-        db.put(&key, &data).expect(&format!("writing {} failed", i));
-    }
-    println!(
-        "rocksdb broadcast-style writes/sec {}",
-        per_second(WRITES, &now.elapsed())
-    );
-
-    // rocksdb
-    let _ignored = DB::destroy(&Options::default(), "rocksdb");
-}
+//fn rocksdb() {
+//    // rocksdb
+//    extern crate rocksdb;
+//    use rocksdb::{Options, DB};
+//
+//    let _ignored = DB::destroy(&Options::default(), "rocksdb");
+//
+//    let mut options = Options::default();
+//    options.create_if_missing(true);
+//
+//    let db = DB::open(&options, "test").expect("first open");
+//
+//    // initialize to a big ass db
+//    let mut data = [0u8; 1024 * 64];
+//    for i in 0..INIT {
+//        let mut key = [0u8; 16];
+//        BigEndian::write_u64(&mut key, i);
+//        BigEndian::write_u64(&mut data, i);
+//        db.put(&key, &data).expect(&format!("writing {} failed", i));
+//    }
+//
+//    let mut rng = rand::thread_rng();
+//    // random order for each chunk of a BLOCK
+//    let now = Instant::now();
+//    for i in 0..WRITES / BLOCK {
+//        let mut keys: Vec<u64> = (i..i + BLOCK).collect();
+//        keys.shuffle(&mut rng);
+//        for i in keys {
+//            let mut key = [0u8; 16];
+//            BigEndian::write_u64(&mut key, i + INIT);
+//            BigEndian::write_u64(&mut data, i + INIT);
+//            db.put(&key, &data).expect(&format!("writing {} failed", i));
+//        }
+//    }
+//
+//    println!(
+//        "rocksdb validator-style writes/sec {}",
+//        per_second(WRITES, &now.elapsed())
+//    );
+//
+//    // append-style
+//    let now = Instant::now();
+//    for i in 0..WRITES {
+//        let mut key = [0u8; 16];
+//        BigEndian::write_u64(&mut key, i + INIT + WRITES);
+//        BigEndian::write_u64(&mut data, i + INIT + WRITES);
+//        db.put(&key, &data).expect(&format!("writing {} failed", i));
+//    }
+//    println!(
+//        "rocksdb broadcast-style writes/sec {}",
+//        per_second(WRITES, &now.elapsed())
+//    );
+//
+//    // rocksdb
+//    let _ignored = DB::destroy(&Options::default(), "rocksdb");
+//}
 
 //fn sled() {
 //    extern crate sled;
@@ -76,7 +79,7 @@ fn rocksdb() {
 //
 //    let tree = Tree::start_default("sled").unwrap();
 //
-//    let data = [0u8; 1024 * 64];
+//    let mut data = [0u8; 1024 * 64];
 //    for i in 0..INIT {
 //        let mut key = [0u8; 16];
 //        BigEndian::write_u64(&mut key, i);
@@ -139,12 +142,13 @@ fn rkv() {
     // (unnamed) store for the environment.
     let store: Store = env.open_or_create_default().unwrap();
 
-    let data = [0u8; 1024 * 64];
+    let mut data = [0u8; 1024 * 64];
     for i in 0..INIT {
         let mut key = [0u8; 16];
         BigEndian::write_u64(&mut key, i);
+        BigEndian::write_u64(&mut data, i);
+        println!("writing {}", i);
         let mut writer = env.write().unwrap();
-
         writer
             .put(store, &key, &Value::Blob(&data))
             .expect(&format!("writing {} failed", i));
@@ -160,6 +164,7 @@ fn rkv() {
         for i in keys {
             let mut key = [0u8; 16];
             BigEndian::write_u64(&mut key, i + INIT);
+            BigEndian::write_u64(&mut data, i + INIT);
             let mut writer = env.write().unwrap();
             writer
                 .put(store, &key, &Value::Blob(&data))
@@ -178,6 +183,7 @@ fn rkv() {
     for i in 0..WRITES {
         let mut key = [0u8; 16];
         BigEndian::write_u64(&mut key, i + INIT + WRITES);
+        BigEndian::write_u64(&mut data, i + INIT + WRITES);
         let mut writer = env.write().unwrap();
         writer
             .put(store, &key, &Value::Blob(&data))
@@ -193,7 +199,7 @@ fn rkv() {
 }
 
 fn main() {
-    rocksdb();
+    //    rocksdb();
     //    sled();
     rkv();
 }
